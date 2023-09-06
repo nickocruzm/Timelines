c98395225be627dd4fb312cfbcb1711307f2d8b6
Nicko Cruz M <nickocruzm@gmail.com>
2023-09-06 01:48:31 -0700

    feat: write()

--- main.cpp
+++ main.cpp
@@ -7,16 +7,21 @@ using namespace std;
     Read and write the contents of the file to textfile
 */
 
+void write(string& , string&);
+
 int main() {
-    // Create an ofstream object to work with files
-    std::ofstream outputFile;
-    outputFile.open("example.txt");
-    string input = "INPUT";
+
+
+
+}
+
+void write(string& fileName, string data){
+    ofstream outputFile;
+    outputFile.open(fileName);
 
     if (outputFile.is_open()){
-        outputFile << input;
+        outputFile << data;
     }
     outputFile.close();
-
 
 }
