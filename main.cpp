2eb2853507c10402e6a1b6ae7289a0a4e6bf4fc9
Nicko Cruz M <nickocruzm@gmail.com>
2023-09-06 10:54:00 -0700

    Read and Write functions updated
    write function handles checking if file exists.
    Defined fileExists() function, used in write function.
    get_input() function defined.

--- main.cpp
+++ main.cpp
@@ -7,21 +7,53 @@ using namespace std;
     Read and write the contents of the file to textfile
 */
 
-void write(string& , string&);
+void write(string& , string);
+bool fileExists(string& fname);
+string get_input();
 
-int main() {
+int main(){
+    string data = get_input();
+    string fileName = "file.txt";
+    write(fileName, data);
+}
 
+bool fileExists(string& fname){
+    fstream file(fname);
+    return file.good();
+}
 
-
+string get_input(){
+    string userInput;
+    getline(cin, userInput);
+    return userInput;
 }
 
 void write(string& fileName, string data){
-    ofstream outputFile;
-    outputFile.open(fileName);
+    /*
+        checks if file exists. 
+            If file does exist, then open file in append mode.
+            If file does not exist, then create file
+        
+        write data into file
+    */
+    ofstream writeFile; //output file stream
 
-    if (outputFile.is_open()){
-        outputFile << data;
+
+
+    if(fileExists(fileName)){
+        // opens file in append mode
+        writeFile.open(fileName, ios::app);
+        // appends data to file
+        writeFile << data;
+    }else{
+        // file does not exist so create file, then append data
+        writeFile.open(fileName);
+        writeFile<< data;
     }
-    outputFile.close();
+    // make sure to add a new line to the data
+    writeFile<< "\n";
+
+    // close file
+    writeFile.close();
 
 }
